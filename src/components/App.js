3c845c908e1383503b743072917296000b953252
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-04-12 03:19:21 +0530

    Initail COmmit

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,48 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderBall: false,
+      posi: 0,
+      ballPosition: { left: "0px" },
     };
+    this.renderChoice = this.renderBallOrButton.bind(this);
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
 
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
+  buttonClickHandler() {
+    this.setState({ renderBall: true });
+  }
+  renderBallOrButton() {
+    if (this.state.renderBall) {
+      return <div className="ball" style={this.state.ballPosition}></div>;
+    } else {
+      return <button onClick={this.buttonClickHandler}>Start</button>;
     }
+  }
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
+  // bind ArrowRight keydown event
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
+  handleKeyDown(e) {
+    if (e.key === "ArrowRight" && this.state.renderBall) {
+      this.setState((prevState) => ({
+        ballPosition: {
+          ...prevState.ballPosition,
+          left: parseInt(prevState.ballPosition.left) + 5 + "px",
+        },
+      }));
     }
+  }
 
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
-    }
+  render() {
+    return <div className="playground">{this.renderBallOrButton()}</div>;
+  }
 }
-
 
 export default App;
